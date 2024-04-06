5103f5d5b2aaae0dd3116d2ec5d1aa748a70fbff
HafizMuhammad0101 <asgharshahzad720@gmail.com>
2024-04-07 02:57:01 +0500

    Configured Backend and also created user schema

---
+++ backend/src/app.ts
@@ -0,0 +1,6 @@
+import { Express } from "express";
+import express from "express";
+
+const app: Express = express();
+
+export default app;
---
+++ backend/src/models/user.model.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+    },
+    email: {
+        type: String,
+    },
+    password: {
+        type: String,
+    },
+    profileImage: {
+        type: String,
+    },
+}, { timestamps: true });
+
+export const User = mongoose.model("User", userSchema);
